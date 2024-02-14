95fe85c7c82b0fc50948104fe6d147e22adf2b0e
kingkazuma7 <your.nasionasio7@gmail.com>
2024-02-14 21:54:01 +0900

    121 click evnet

--- dist/food-app.js
+++ dist/food-app.js
@@ -2,6 +2,29 @@
 class Score {
 }
 class Food {
+    constructor(elemet) {
+        this.elemet = elemet;
+        elemet.addEventListener('click', this.clickHnader.bind(this));
+    }
+    clickHnader() {
+        this.elemet.classList.toggle('food--active');
+    }
 }
 class Foods {
+    get activeElements() {
+        this.elements.forEach(item => {
+            if (item.classList.contains('food--active')) {
+                this._activeElem.push(item);
+            }
+        });
+        return this._activeElem;
+    }
+    constructor() {
+        this.elements = document.querySelectorAll('.food');
+        this._activeElem = [];
+        this.elements.forEach(item => {
+            new Food(item);
+        });
+    }
 }
+const foods = new Foods();
--- src/food-app.ts
+++ src/food-app.ts
@@ -1,3 +1,27 @@
 class Score { }
-class Food { }
-class Foods { }
+class Food {
+  constructor(public elemet: HTMLDivElement) {
+    elemet.addEventListener('click', this.clickHnader.bind(this));
+  }
+  clickHnader() {
+    this.elemet.classList.toggle('food--active');
+  }
+}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  private _activeElem: HTMLDivElement[] = [];
+  get activeElements() {
+    this.elements.forEach(item => {
+      if (item.classList.contains('food--active')) {
+        this._activeElem.push(item);
+      }
+    });
+    return this._activeElem;
+  }
+  constructor() {
+    this.elements.forEach(item => {
+      new Food(item);
+    });
+  }
+}
+const foods = new Foods();
